8ead6b1e2fd255e68a51c6473743574829f9c488
Manajit Halder <manajit.halder@gmail.com>
2025-01-07 08:24:26 +0530

    web wallet web3Projects + added button active color change

--- web3/WebBasedWallet/web-wallet/src/components/Login.tsx
+++ web3/WebBasedWallet/web-wallet/src/components/Login.tsx
@@ -4,7 +4,7 @@ const Login = () => {
     return (
         <>
             <div className="text-gray-50 flex justify-center items-center h-screen">
-                <div className=" bg-gray-950 flex flex-col justify-center items-center w-96 p-10 rounded-lg text-center">
+                <div className=" bg-gray-800 flex flex-col justify-center items-center w-96 p-10 rounded-3xl text-center shadow-2xl">
 
                     <div className="text-2xl pb-8 w-96">
                         Signup Page
@@ -13,10 +13,29 @@ const Login = () => {
                         <input type="text" placeholder="username" className="p-2 rounded-lg w-80 mb-4" />
                     </div>
                     <div className="pb-4 w-96">
-                        <input type="password" placeholder="password" className="p-2 rounded-lg mb-4 w-80" />
+                        <input
+                            type="password"
+                            placeholder="password"
+                            className="p-2 rounded-lg mb-4 w-80"
+                        />
                     </div>
                     <div className="pb-4 w-96">
-                        <button className="p-2 rounded-lg bg-gray-400 text-gray-950 w-80">Signup</button>
+                        <button
+                            className={`
+                                p-2 rounded-lg w-80
+                                bg-gray-400 text-gray-950                            
+                                active:bg-gray-600 active:text-gray-50
+                                shadow-gray-200 shadow-sm
+                            `}
+                            onClick={() => {
+
+                            }}
+                            onFocus={() => {
+
+                            }}
+                        >
+                            Signup
+                        </button>
                     </div>
                 </div>
             </div >
